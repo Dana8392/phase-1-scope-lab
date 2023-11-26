151560ae17efe587ad76e3fa08a4dcb58da3a455
Dana Maraqa <dana.maraqa@hotmail.com>
2023-11-26 14:52:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  
+  var bestCustomer;
+
+  function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+
+  const leastFavoriteCustomer = 'initial least favorite';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new least favorite';
+  }
